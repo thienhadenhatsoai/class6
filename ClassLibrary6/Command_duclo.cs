7e34d78d230e7512a5309f9a3dea41899fee59c9
thienhadenhatsoai <ken.watashino@gmail.com>
2023-07-04 10:57:30 +0700

    upload 4/7

---
+++ ClassLibrary6/Command_duclo.cs
@@ -0,0 +1,47 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using System.Data;
+
+namespace ClassLibrary6
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    class Command_duclo : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            //Tìm farmily
+
+            try
+            {
+                Reference r = uidoc.Selection.PickObject(Autodesk.Revit.UI.Selection.ObjectType.Element);
+                ElementId elementId = r.ElementId;
+                Element element = doc.GetElement(elementId);
+                if (element != null)
+                { 
+
+                }
+                return Result.Succeeded;
+            }
+            catch (Exception ex)           
+             {
+                message = ex.Message;
+                return Result.Failed;
+            }
+
+
+
+
+        }
+
+    }
+}
